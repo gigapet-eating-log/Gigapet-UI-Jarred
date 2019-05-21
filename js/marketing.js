c169bf67a568d7fd10569c0348689c4beef655a6
unknown <stanj89@gmail.com>
2019-05-21 16:01:28 -0400

    Animations added. layout improved.

--- js/marketing.js
+++ js/marketing.js
@@ -1,1 +1,18 @@
 /*js*/
+
+const mainContent = document.querySelector(".main-content");
+
+const learnMore = document.querySelector(".learn-more");
+
+const childHealthfacts = document.querySelector(".child-healthfacts");
+
+const footer = document.querySelector("footer");
+
+//When Learn More button is pressed, main-content is revealed and the page is scrolled to the content.
+learnMore.addEventListener("click", function() {
+  mainContent.style.display = "flex";
+  footer.style.display = "flex";
+  mainContent.scrollIntoView({
+    behavior: "smooth"
+  });
+});
